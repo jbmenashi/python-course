902af635072da301dc577e3d3f8a565f83b6c72e
jbmenashi <jbmenashi@gmail.com>
2019-03-12 17:32:56 -0400

    fancy list methods

---
+++ fancy_list_methods.py
@@ -0,0 +1,15 @@
+list = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+list.index("red") #finds the index
+list.index("green", 3, 5) #can do starting and ending indexes
+
+list.count("red") #number of times it's in the list
+
+list.reverse() #obvi
+
+list.sort() #sorts! alphabetically
+
+list.join(' ') #same as JS
+
+
+print(list)
