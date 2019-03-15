6d4bb7863264863a8f0063f1ad4b46c09743e90c
jbmenashi <jbmenashi@gmail.com>
2019-03-15 10:16:27 -0400

    tuples and sets

---
+++ tuples_and_sets.py
@@ -0,0 +1,14 @@
+tuple1 = (1, 2, 3, 4) # its a list with parenthesis, and it's IMMUTABLE!!!!
+# tuples are faster than lists, so if you know it's not gonna change use it
+tuple1[0] # 1
+tuple1[3] # 4
+
+#can be used as keys in dictionaries
+# iterating is the same
+
+set1 = set({1, 2, 3, 4, 5})
+
+# no duplicates, no indexes, no keys, no values
+# iterating is the same
+
+#good way to remove duplicates
