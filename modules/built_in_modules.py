aed6634e962dd6fc0461f19408974e7ecb3aca7d
Jacob <jbmenashi@gmail.com>
2019-03-30 16:43:16 -0400

    more work with modules

--- modules/built_in_modules.py
+++ modules/built_in_modules.py
@@ -7,7 +7,7 @@ randooo.choice(["A", "B", "C"])
 # but you don't really need to import everything
 
 from random import randint # you can import specific things
-from random import * # this still imports everything BUT now you don't need random. before functions
+# from random import * # this still imports everything BUT now you don't need random. before functions
 randint(1, 100)
 
 
---
+++ modules/colors.py
@@ -0,0 +1,10 @@
+# to install stuff, python -m pip install ______
+
+import termcolor
+
+help(termcolor)
+ #to look at all the functions you can use
+
+text = termcolor.colored("Hello", color="magenta")
+print(text)
+
