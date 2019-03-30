815dfdc5b2f9d3520dcd9c56c9783dc4ec4530be
Jacob <jbmenashi@gmail.com>
2019-03-30 15:48:26 -0400

    built in modules

---
+++ modules/built_in_modules.py
@@ -0,0 +1,13 @@
+# There's lots of built-in modules
+
+import random as randooo # you can alias any module
+
+randooo.choice(["A", "B", "C"])
+
+# but you don't really need to import everything
+
+from random import randint
+randint(1, 100)
+
+
+
