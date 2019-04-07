041a6bb34d7572b36e88db92642170e54173595e
Jacob <jbmenashi@gmail.com>
2019-04-07 18:47:01 -0400

    deck of cards

---
+++ deck_of_cards/card.py
@@ -0,0 +1,44 @@
+from random import shuffle
+
+class Card:
+   def __init__(self, suit, value):
+      self.suit = suit
+      self.value = value 
+
+   def __repr__(self):
+      return f"{self.value} of {self.suit}"
+
+
+class Deck:
+   def __init__(self):
+      suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+      values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+      self.cards = [Card(value, suit) for suit in suits for value in values]
+
+   def count(self):
+      return len(self.cards)
+
+   def __repr__(self):
+      return f"There are {self.count()} cards left in the deck"
+
+   def _deal(self, num):
+      count = self.count()
+      removal = min([count, num]) # so it can only remove cards when it's possible
+      if count == 0:
+         raise ValueError("No cards left!")
+      hand = self.cards[-removal:]
+      self.cards = self.cards[:-removal]
+      return hand
+
+   def deal_card(self):
+      return self._deal(1)[0]
+      
+   def deal_hand(self, hand_size):
+      return self._deal(hand_size)
+
+   def shuffle(self):
+      if self.count() < 52:
+         raise ValueError("Only full decks can be shuffled")
+      shuffle(self.cards)
+      return self
