c671a04ca197b38b152a1b7bf155e1794f430936
Jacob <jbmenashi@gmail.com>
2019-04-21 17:42:30 -0400

    practice with files

---
+++ input-output/writing_files.py
@@ -0,0 +1,9 @@
+with open("write.txt", "w") as file:
+   file.write("Look I'm writing in a file\n")
+
+# with open("write.txt", "a") as file:
+#    file.write("This won't get rid of the above text\n")
+
+# with open("write.txt", "r+") as file:
+#    file.seek(10)
+#    file.write("LOOK AT ME")
