923efb6431685d1ae84fcf89aeecf7039ea80c3c
Jacob <jbmenashi@gmail.com>
2019-04-23 17:59:28 -0400

    scraping 1

---
+++ web_scraping/bs_basics.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>First HTML Page</title>
+</head>
+<body>
+  <div id="first">
+    <h3 data-example="yes">hi</h3>
+    <p>more text.</p>
+  </div>
+  <ol>
+    <li class="special">This list item is special.</li>
+    <li class="special">This list item is also special.</li>
+    <li>This list item is not special.</li>
+  </ol>
+  <div data-example="yes">bye</div>
+</body>
+</html>
+"""
+
+soup = BeautifulSoup(html, "html.parser")
+d = soup.select("[data-example]")
+e = soup.find(id="first")
+f = soup.find_all(class_="special") # or soup.select(".special")
+g = soup.find_all(attrs={ "data-example": "yes"})
+print(soup)
+print(d)
+print(e)
+print(g)
