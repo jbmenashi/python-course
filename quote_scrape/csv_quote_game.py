479937ea2d8247910672750defaa96ac0923923d
Jacob <jbmenashi@gmail.com>
2019-06-04 15:33:37 -0400

    complete quote scraper game

--- quote_scrape/csv_quote_game.py
+++ quote_scrape/csv_quote_game.py
@@ -1,4 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+from random import choice
+from csv import DictReader
 
+BASE_URL = "http://quotes.toscrape.com/"
+
+def read_quotes(filename):
+   with open(filename, "r") as file:
+      csv_reader = DictReader(file)
+      return list(csv_reader)
 
 def start_game(quotes):
    quote = choice(quotes)
@@ -9,7 +20,8 @@ def start_game(quotes):
    guess = ""
 
    while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
-      guess = input(f"Who said this quote? Guesses remaining: {remaining_guesses} ")
+      guess = input(
+          f"Who said this quote? Guesses remaining: {remaining_guesses} ")
       if guess.lower() == quote["author"].lower():
          print("You Got It!")
          break
@@ -19,11 +31,14 @@ def start_game(quotes):
          soup = BeautifulSoup(res.text, 'html.parser')
          birthdate = soup.find(class_="author-born-date").get_text()
          birthplace = soup.find(class_="author-born-location").get_text()
-         print(f"Here's a hint: The author was born in {birthplace} on {birthdate}")
+         print(
+             f"Here's a hint: The author was born in {birthplace} on {birthdate}")
       elif remaining_guesses == 2:
-         print(f"Another hint: Their first name starts with {quote['author'][0]}")
+         print(
+             f"Another hint: Their first name starts with {quote['author'][0]}")
       elif remaining_guesses == 1:
-         print(f"One more hint: Their last name starts with {quote['author'].split()[1][0]}")
+         print(
+             f"One more hint: Their last name starts with {quote['author'].split()[1][0]}")
       else:
          print(f"Out of Guesses! It was {quote['author']}")
 
@@ -36,5 +51,5 @@ def start_game(quotes):
    else:
       print("Okay goodbye")
 
-quotes = scrape_quotes()
+quotes = read_quotes("quotes.csv")
 start_game(quotes)
