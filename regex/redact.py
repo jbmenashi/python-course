2f20f7fc7fd8e79ee997f875305c30132f409d02
Jacob <jbmenashi@gmail.com>
2019-06-04 19:07:11 -0400

    fixed

--- regex/redact.py
+++ regex/redact.py
@@ -2,5 +2,5 @@ import re
 text = "Last night Mrs. Daisy and Mr. White murdered Ms. Chow"
 
 pattern = re.compile(r'(Mr\.|Mrs\.|Ms\.) ([a-z])[a-z]+', re.I)
-result = pattern.sub("\g<1> \g<2>", text)
+result = pattern.sub(r'\g<1> \g<2>', text)
 print(result)
